06fcff5109985082e2d6c27106d12d0ed3ba84eb
Jon Coe <jon@seatgeek.com>
2019-01-01 19:45:07 -0500

    p08 cleanup

--- src/main/scala/problems/Problem08.scala
+++ src/main/scala/problems/Problem08.scala
@@ -4,33 +4,28 @@ import common.BaseProblem
 
 object Problem08 extends BaseProblem(8) {
 
-    private val full : Seq[Int] = readInput().next().split(' ').map(_.toInt)
-
     case class Node(children: Seq[Node], metadata: Seq[Int]) {
       def metadataSum: Int = metadata.sum + children.map(_.metadataSum).sum
       def value: Int =
-        if (children.isEmpty)
-          metadataSum
-        else
-          metadata.map(m => if (0 < m && m < children.length + 1) children(m - 1).value else 0).sum
+        children match {
+          case _ :: _ => metadata.map(m => if (0 < m && m < children.length + 1) children(m - 1).value else 0).sum
+          case Nil => metadataSum
+        }
     }
 
-    def buildTree(intSeq: Seq[Int]) : (Seq[Int], Node) = {
-      val (numChildren, numMetadata, content) = (intSeq.head, intSeq(1), intSeq.drop(2))
+    def buildTree(intSeq: List[Int]) : (List[Int], Node) = {
+      val numChildren :: numMetadata :: content = intSeq
       val (rem, children) = (0 until numChildren).foldLeft(content, Seq.empty[Node])(
-        (t, _) => {
-          val x = buildTree(t._1)
-          (x._1, t._2 :+ x._2)
+        (acc, _) => {
+          val x = buildTree(acc._1)
+          (x._1, acc._2 :+ x._2)
         }
       )
       val (metadata, extra) = rem.splitAt(numMetadata)
       (extra, Node(children, metadata))
     }
 
-  val tree = buildTree(full)._2
+  private val tree = buildTree(readInput().next().split(' ').map(_.toInt).toList)._2
   def solutionA = tree.metadataSum
   def solutionB = tree.value
-
-//  def solutionA = buildTree(fullIntList).metadataSum
-//  def solutionB = ""
 }
