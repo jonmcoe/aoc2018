ffa0067bf5fc14e2d74cb231ed630b0edb8cfaed
Jon Coe <jon@seatgeek.com>
2019-01-13 12:15:58 -0500

    p11 change to foldLeft

--- src/main/scala/problems/Problem11.scala
+++ src/main/scala/problems/Problem11.scala
@@ -16,21 +16,19 @@ object Problem11 extends BaseProblem (11) {
   private val cellTable: IndexedSeq[IndexedSeq[Int]] = (1 to gridSize).map(y => (1 to gridSize).map(x => cellScore(x, y)))
 
   def bestSquareScore(x: Int, y: Int, maxSize: Int): (Int, Int) = {
-    var best = (Int.MinValue, -1)
-    var incremental = 0
-
     val validSizeLimit = List(gridSize - x + 1, gridSize - y + 1, maxSize).min
-
-    // this could probably be done with fold instead of mutables
-    for(i <- 0 until validSizeLimit) {
-      val addition1 = cellTable(y + i - 1).slice(x - 1, x + i - 1)
-      val addition2 = (y to y + i).map(cy => cellTable(cy - 1)(x + i - 1))
-      incremental += addition1.sum + addition2.sum
-      if (incremental > best._1) {
-        best = (incremental, i + 1)
+    val best = (0 until validSizeLimit).foldLeft(Int.MinValue, 0, 0) {
+      (acc, i) => {
+        val addition1 = cellTable(y + i - 1).slice(x - 1, x + i - 1)
+        val addition2 = (y to y + i).map(cy => cellTable(cy - 1)(x + i - 1))
+        val cur = acc._2 + addition1.sum + addition2.sum
+        if (cur > acc._1)
+          (cur, cur, i + 1)
+        else
+          (acc._1, cur, acc._3)
       }
     }
-    best
+    (best._1, best._3)
   }
 
   def allStartingPointBestScores(maxSize: Int) = for {
