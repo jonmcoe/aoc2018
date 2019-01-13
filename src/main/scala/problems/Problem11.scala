2118333bde937cea04b4a6358608243d180af9d5
Jon Coe <jon@seatgeek.com>
2019-01-13 12:00:25 -0500

    p11 ugly speedup complete

--- src/main/scala/problems/Problem11.scala
+++ src/main/scala/problems/Problem11.scala
@@ -15,20 +15,29 @@ object Problem11 extends BaseProblem (11) {
 
   private val cellTable: IndexedSeq[IndexedSeq[Int]] = (1 to gridSize).map(x => (1 to gridSize).map(y => cellScore(x, y)))
 
-  def squareScore(x: Int, y: Int, size: Int): Int = {
-    val included = for {
-      i <- 0 until size
-      j <- 0 until size
-    } yield (x + i, y + j)
-    included.map(t => cellTable(t._1 - 1)(t._2 - 1)).sum
+  def bestSquareScore(x: Int, y: Int, maxSize: Int): (Int, Int) = {
+    var best = (Int.MinValue, -1)
+    var incremental = 0
+
+    val validSizeLimit = List(gridSize - x + 1, gridSize - y + 1, maxSize).min
+
+    // this could probably be done with fold instead of mutables
+    for(i <- 0 until validSizeLimit) {
+      val addition1 = cellTable(x + i - 1).slice(y -1, y+i-1)
+      val addition2 = (x to x + i).map(cx => cellTable(cx-1)(y+i-1))
+      incremental += addition1.sum + addition2.sum
+      if (incremental > best._1) {
+        best = (incremental, i + 1)
+      }
+    }
+    best
   }
 
-  def allScores(maxSize: Int) = for {
-    size <- 1 to maxSize
-    x <- 1 to gridSize - size + 1
-    y <- 1 to gridSize - size + 1
-  } yield ((x, y, size), squareScore(x, y, size))
+  def allStartingPointBestScores(maxSize: Int) = for {
+    x <- 1 to gridSize
+    y <- 1 to gridSize
+  } yield ((x, y), bestSquareScore(x, y, maxSize))
 
-  def solutionA= allScores(3).maxBy(_._2)
-  def solutionB = allScores(20).maxBy(_._2) // slow. maybe should fix origin and build size/score incrementally
+  def solutionA= allStartingPointBestScores(3).filter(_._2._2 == 3).maxBy(_._2._1)
+  def solutionB = allStartingPointBestScores(300).maxBy(_._2._1)
 }
